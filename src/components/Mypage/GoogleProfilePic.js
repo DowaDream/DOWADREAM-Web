efa6ed683aa3dfe6701f8ce4f9ea7dd60b79c05b
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-08-29 02:27:04 +0900

    [CHORE] 마이페이지 수정

--- src/components/Mypage/GoogleProfilePic.js
+++ src/components/Mypage/GoogleProfilePic.js
@@ -4,21 +4,23 @@ import styled from 'styled-components';
 import pic from '../../프로필.jpg';
 
 const GoogleProfilePic = (props) => {
-    const { size } = props;
+    const { size, url=pic } = props;
   
     return (
-      <BackgroundProfile size={size}></BackgroundProfile>
+      <BackgroundProfile size={size} pic={url}></BackgroundProfile>
     )
   }
 
 export default GoogleProfilePic;
 const BackgroundProfile = styled.div`
-width: ${props => props.size}px;
-height: ${props => props.size}px;
-display: flex;
-flex-direction: row;
-align-items: flex-start;
-padding: 0px;
-background: url(${pic});
-border-radius: 50%;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  background: url(${props=> props.pic});
+  background-size: cover; /* 이미지를 컨테이너에 맞게 늘립니다 */
+  background-position: center; /* 이미지를 중앙으로 정렬합니다 */ 
+  border-radius: 50%;
 `;
